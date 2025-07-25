43eace92975633d50793e3bb2521a1d12f395e61
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2025-07-25 15:42:01 +0300

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header/Header';
 import MainText from './components/MainText/MainText';
 import MainEmojis from './components/MainEmojis/MainEmojis';
+import Footer from './components/Footer/Footer';
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Header></Header>
 			<MainText></MainText>
 			<MainEmojis></MainEmojis>
+			<Footer></Footer>
 		</>
 	);
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import './style.css';
+
+function Footer() {
+	return (
+		<>
+			<h1 className="footer-title display-3">Hot Emojis</h1>
+			<div className="emoji-list">
+				<div className="emoji-list-2">
+					<p className="display-3 emoji-block">💦</p>
+					<p className="display-3 emoji-block">😱</p>
+					<p className="display-3 emoji-block">😡</p>
+					<p className="display-3 emoji-block">😩</p>
+					<p className="display-3 emoji-block">🫣</p>
+					<p className="display-3 emoji-block">🥵</p>
+					<p className="display-3 emoji-block">😤</p>
+					<p className="display-3 emoji-block">🥴</p>
+					<p className="display-3 emoji-block">🍑</p>
+					<p className="display-3 emoji-block">😏</p>
+					<p className="display-3 emoji-block">🤯</p>
+					<p className="display-3 emoji-block">🥶</p>
+					<p className="display-3 emoji-block">🤪</p>
+					<p className="display-3 emoji-block">🤩</p>
+					<p className="display-3 emoji-block">🤣</p>
+					<p className="display-3 emoji-block">🫠</p>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default Footer;
